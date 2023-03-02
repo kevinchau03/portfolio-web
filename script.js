c85e848a7ec9e29819b50deab7cf9b1e49b47859
Kevin Chau <kckevinchau11@gmail.com>
2023-03-01 23:57:06 -0500

    Changed Sizing

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-// Disable scrolling by default
-document.body.style.overflow = 'hidden';
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
 
-console.log("Hello World");
-
-// Enable scrolling when <a> tag is clicked
-document.querySelector('a').addEventListener('click', function() {
-  document.body.style.overflow = '';
-});
+// To check the scroll position on page load
+reveal();
