5d44ccdc9a557a1a35c03220c0c1173cac31de47
Kevin Chau <kchau57@uwo.ca>
2023-04-28 16:29:25 -0400

    adding projects to projects screen

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ window.addEventListener('scroll', function() {
     landingContainer.classList.add('active');
   }
 });
+
+window.addEventListener('scroll', function() {
+  const itemContainer = document.querySelector('.item-container');
+  const projectsScreenHeight = document.querySelector('.projects-screen').offsetHeight;
+  const scrollTop = window.scrollY;
+  if (scrollTop < projectsScreenHeight / 2) {
+    itemContainer.classList.add('hidden');
+    itemContainer.classList.remove('active');
+  } else {
+    itemContainer.classList.remove('hidden');
+    itemContainer.classList.add('active');
+  }
+});
