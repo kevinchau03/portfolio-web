9732035b90788c842ced6af81063a56d86d32817
Kevin Chau <kchau57@uwo.ca>
2023-05-03 16:51:29 -0400

    Added new animations and icons

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 window.addEventListener('scroll', function() {
   const landingContainer = document.querySelector('.landing-container');
   const landingScreenHeight = document.querySelector('.landing-screen').offsetHeight;
-  const scrollTop = window.scrollY;
+
   const projectsContainer = document.querySelector('.projects-container');
   const projectScreenHeight = document.querySelector('.projects-screen').offsetHeight;
   const projectScreenPosition = document.querySelector('.projects-screen').offsetTop;
+
+  const aboutContainer = document.querySelector('.about-container');
+  const experienceScreenHeight = document.querySelector('.experience-screen').offsetHeight;
+  const experienceScreenBottom = (experienceScreenHeight + aboutContainer.offsetTop / 3);
+
+  const scrollTop = window.scrollY;
+  console.log(scrollTop,  experienceScreenBottom);
+  
   if (scrollTop > landingScreenHeight / 4) {
     landingContainer.classList.add('hidden');
     landingContainer.classList.remove('active');
   } else {
     landingContainer.classList.remove('hidden');
     landingContainer.classList.add('active');
+  }
+
+  if (scrollTop < experienceScreenHeight / 2) {
+    aboutContainer.classList.add('hidden');
+    aboutContainer.classList.remove('active');
+  } else if (scrollTop > experienceScreenBottom) {
+    aboutContainer.classList.add('hidden');
+    aboutContainer.classList.remove('active');
+  } else {
+    aboutContainer.classList.add('active');
+    aboutContainer.classList.remove('hidden');
   }
 
   if (scrollTop > (projectScreenPosition / 3) + projectScreenHeight && scrollTop < (projectScreenHeight + projectScreenPosition) - (projectScreenHeight / 1.75)) {
